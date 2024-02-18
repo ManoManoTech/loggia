23089b914a154ff2df3a3b1ac539f64c941452f6
Gabriel Dugny <gabriel.dugny@manomano.com>
2024-02-18 15:08:46 +0100

    fix: allow lowercase levels in LOGGIA_SUB_LEVEL/set_logger_level

--- loggia/utils/strutils.py
+++ loggia/utils/strutils.py
@@ -9,3 +9,11 @@ _snake_case_re = re.compile(r"(?<!^)(?=[A-Z])")
 # XXX Tests
 def to_snake_case(s: str) -> str:
     return _snake_case_re.sub("_", s).lower()
+
+
+def clean_log_level(level: str | int) -> str | int:
+    if isinstance(level, str):
+        level = level.upper()
+        if level.isdigit():
+            level = int(level)
+    return level
